e9119de5e73c04b1f7850fd886c3ff6284428da6
Niall <niallpepper@gmail.com>
2023-11-30 19:39:34 -0500

    Testing to see if a new web build fixes the blank screen.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:photo_website/pages/main.dart';
+import 'package:photo_website/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
